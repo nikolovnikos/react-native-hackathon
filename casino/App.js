6e6d6d94177b3a9d2962c4de0ba3376516980209
nikichanko <nikikolovnikos@gmail.com>
2017-12-14 12:15:16 +0200

    fix re

--- casino/App.js
+++ casino/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'reactnative';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
